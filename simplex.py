d741ac34908d46c78f5c2e01df67eac2b39e04ee
Giovanna Neves <giovanna.neves@maxmilhas.com.br>
2018-05-06 16:34:50 -0300

    Simplex primal - fluxo padrao funcionando

--- simplex.py
+++ simplex.py
@@ -1,8 +1,22 @@
 import numpy as np 
+from pdb import set_trace
+import copy
 import math
 
+def put_identity_matrix(matrix,position,size):
+	adicional_row = np.zeros(size)
+	identity = np.identity(size)
+	sub_matrix = np.insert(identity, 0, adicional_row, axis=0)
+
+	arrays_list = []
+	for line_index in range(matrix.shape[0]):
+		extended_row = np.insert(matrix[line_index], position, sub_matrix[line_index])
+		arrays_list.append(extended_row)
+	return np.array(arrays_list)
+
 def parse_to_fpi(matrix):
-	print(oi)
+	matrix_A_lines = (matrix.shape[0]-1)
+	return put_identity_matrix(matrix,matrix_A_lines,matrix_A_lines)
 
 def canonical_form(matrix):
 	if(not verify_canonical_form(matrix)):
@@ -19,15 +33,21 @@ def put_canonical_form(matrix):
 	pass
 
 def put_tableux_form(matrix):
-	#add identity matrix in left side
-	#matriz[1:] . (-) (multiplica primeira linha por -1)
-	pass
+	matrix_A_lines = (matrix.shape[0]-1)
+	matrix = put_identity_matrix(matrix,0,matrix_A_lines)
+	matrix[0,:] = (-1)*matrix[0,:] 
+	return matrix
 
-def find_c_positive(matrix): 
-	begin_A_columns = matrix.shape[0]
+def put_pl_form(matrix):
+	begin_C_columns = matrix.shape[0]-1
+	end_C_columns = matrix.shape[1]-1
+	matrix[0,begin_C_columns:end_C_columns] = (-1)*matrix[0,begin_C_columns:end_C_columns] 
+
+def find_c_negative(matrix): 
+	begin_A_columns = matrix.shape[0]-1
 	end_A_columns = matrix.shape[1]
 	for index in range(begin_A_columns, end_A_columns):
-		if matrix[0,index] >= 0:
+		if matrix[0,index] < 0:
 			return index
 	return (-1)
 
@@ -60,7 +80,7 @@ def find_pivot_dual_simplex(matrix,b_index):
 	min_value = math.inf
 	min_index = None
 
-	begin_A_columns = matrix.shape[0] 
+	begin_A_columns = matrix.shape[0] -1
 	end_A_columns = matrix.shape[1]
 
 	for index in range(begin_A_columns,end_A_columns):
@@ -80,29 +100,58 @@ def pivoting(matrix,line_index, column_index):
 			matrix[index,:] = matrix[index,:]/matrix[line_index,column_index]
 		else:
 			matrix[index,:] = matrix[line_index,:]*( (-matrix[index,column_index])/matrix[line_index,column_index])+matrix[index,:]	
+	
+	print("\nPivoteamento:("+str(line_index)+","+str(column_index)+")\n")
 	print (matrix)
-# def main():
-f = open('teste1.txt', 'r')
 
-linhas 	= 4  #f.readline()
-colunas = 7#f.readline()
+def verify_state(matrix):	
+	begin_C_columns = matrix.shape[0]-1
+	end_C_columns = matrix.shape[1]-1
+	if (all( i >=0 for i in matrix[0,begin_C_columns:end_C_columns]) ) and (all(i >=0 for i in matrix[1:,-1]) ):
+		return True
+	elif (all( i <=0 for i in matrix[0,begin_C_columns:end_C_columns]) ):
+		return None #passar para simplex dual
+	else:
+		return False #continua simplex primal
 
-# texto = f.readline()
-# print(texto+'\n')
-matrix = np.array([[0,0,0,-3,-2,-4,0,0,0,0],[1,0,0,1,1,2,1,0,0,4],[0,1,0,2,0,3,0,1,0,5],[0,0,1,2,1,3,0,0,1,7]],dtype=float)
+def primal_simplex(matrix):
+	#set_trace()
+	c_index = find_c_negative(matrix) #tratar caso (-1) - nao tem c negativo
+	line_index =  find_pivot_primal_simplex(matrix,c_index)
+	pivoting(matrix,line_index,c_index)
+	state = verify_state(matrix)
+	if(state):
+		return
+	elif(state is None):
+		#executar simples dual
+		pass	
+	else:
+		primal_simplex(matrix)
 
-# 0 0 0| 3 2 4 0 0 0 | 0
-# - - - - - - - - - - - - - 
-# 1 0 0| 1 1 2 1 0 0 | 4
-# 0 1 0| 2 0 3 0 1 0 | 5
-# 0 0 1| 2 1 3 0 0 1 | 7
-#
 
-canonical_form(matrix)
+def main():
+
+#f = open('teste1.txt', 'r')
+
+#linhas 	= 4  #f.readline()
+#colunas = 7#f.readline()
+
+	matrix = np.array([[3,2,4,0],[1,1,2,4],[2,0,3,5],[2,1,3,7]],dtype=float)
+
+	print("\nMatriz inicial:\n")
+	print(matrix)
+	matrix = parse_to_fpi(matrix)
+	print(matrix)
+	matrix = put_tableux_form(matrix)
+	print(matrix)
+	#primal_simplex(matrix)
+
+	#primal_simplex(matrix)
+#	print("\nMatriz final: \n")
+#	put_pl_form(matrix)#mudar o formato para que o valor objetivo nao fique negativo na hora errada
+#	print(matrix)
+
+
 # identidade = np.identity(2,float)
-# print(identidade)
-# matriz = np.array(texto)
-# print(matriz[0,2])
-# print('\n')	
-# type(matriz)
 
+main()
